94b351778b896273e5a28b476450788691124a96
Unhynged <unhynged@protonmail.com>
2022-06-21 16:11:16 +1000

    Modified index.js to display different books, Props -> Props Destructuring
    Also added changes to index.css

--- src/index.js
+++ src/index.js
@@ -1,32 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-// CSS Basics
 import "./index.css";
+
+// Setup Variables
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/81F30JDZU9L._AC_UL604_SR604,400_.jpg",
+  title: "Why a Daughter Needs a Dad",
+  author: "Gregory E. Lang",
+};
+
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/81a5KHEkwQL._AC_UL604_SR604,400_.jpg",
+  title: "Oh, the Places You'll Go!",
+  author: "Dr. Seuss",
+};
+
+const thirdBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/61sDK3mWV9L._AC_UL604_SR604,400_.jpg",
+  title: "I'd Like to Play Alone, Please: Essays",
+  author: "Tom Segura",
+};
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+      <Book
+        img={thirdBook.img}
+        title={thirdBook.title}
+        author={thirdBook.author}
+      />
     </section>
   );
 }
 
-const author = 'Gregory E. Lang'
-const Book = () => {
-  const title = "Why a Daughter Needs a Dad";
+const Book = (props) => {
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/81F30JDZU9L._AC_UL604_SR604,400_.jpg"
-        alt=""
-        width="280px"
-        height="200px"
-      />
-      <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
-      {/* <p>This is how you comment out in JSX */}
-      <p>{6+6}</p>
+      <img className="img" src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
