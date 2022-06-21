58ff9af5845638cab50770b41d1347e7ee759d5f
Unhynged <unhynged@protonmail.com>
2022-06-21 14:45:11 +1000

    Modified index.js, JSX Rules -> Nested Components and Tools
    Demonstrating various methods of writing essentially the same functions

--- src/index.js
+++ src/index.js
@@ -1,34 +1,56 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-// stateless functional component
-// always return JSX
+// JSX Rules
+// return single element - Inside the element that we're returning, we can get extremely creative; you could return a whole website. Keep in mind though that we're still rendering the HTML.
+// div / section / article or Fragment - You have the option of using proper HTML semantics or you can use a fragment.
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
+
+// function Greeting() {
+//    return (
+//     <div className="">
+//       <h3>Hello World</h3>
+//       <ul>
+//         <li>
+//             <a href="#">Hello World</a>
+//         </li>
+//         <img src="" alt="" />
+//         <input type="text" name="" id="" />
+//       </ul>
+//     </div>
+//   );
+// }
+
+// function Greeting() {
+//    return (
+//     <React.Fragment>
+//         <div>
+//             <h3>Hello World</h3>
+//             <ul>
+//                 <li>
+//                     <a href='#'>Hello World</a>
+//                 </li>
+//             </ul>
+//         </div>
+//         <div></div>
+//     </React.Fragment>
+//   );
+// }
+
+//  function Greeting() {
+//     return <h1>hello world</h1>;
+// }
 
 function Greeting() {
-  return (
-    <div>
-        <h1>Hello World</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>hello World</h1>
+        </div>
+    )
 }
-
-/**
- * The below two examples are alternative methods of doing the above but are ultimately not recommended. 
- */
-
-// const Greeting = () => {
-//   return React.createElement("h1", {}, "Hello World");
-// };
-
-// const Greeting = () => {
-//     return React.createElement(
-//         'div',
-//         {},
-//         React.createElement('h1', {}, 'hello world')
-//     );
-// };
-
-// ReactDom.render(<Greeting />, document.getElementById("root"));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
