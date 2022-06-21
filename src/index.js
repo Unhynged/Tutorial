cb283fabb19e57e64f3957c6ae56cc357d7b3073
Unhynged <unhynged@protonmail.com>
2022-06-21 15:01:22 +1000

    Modified index.js, Nested Components -> Mini Book Project

--- src/index.js
+++ src/index.js
@@ -1,56 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// JSX Rules
-// return single element - Inside the element that we're returning, we can get extremely creative; you could return a whole website. Keep in mind though that we're still rendering the HTML.
-// div / section / article or Fragment - You have the option of using proper HTML semantics or you can use a fragment.
-// use camelCase for html attribute
-// className instead of class
-// close every element
-// formatting
-
-// function Greeting() {
-//    return (
-//     <div className="">
-//       <h3>Hello World</h3>
-//       <ul>
-//         <li>
-//             <a href="#">Hello World</a>
-//         </li>
-//         <img src="" alt="" />
-//         <input type="text" name="" id="" />
-//       </ul>
-//     </div>
-//   );
-// }
-
-// function Greeting() {
-//    return (
-//     <React.Fragment>
-//         <div>
-//             <h3>Hello World</h3>
-//             <ul>
-//                 <li>
-//                     <a href='#'>Hello World</a>
-//                 </li>
-//             </ul>
-//         </div>
-//         <div></div>
-//     </React.Fragment>
-//   );
-// }
-
-//  function Greeting() {
-//     return <h1>hello world</h1>;
-// }
+// Nested Components, React Tools
 
 function Greeting() {
-    return (
-        <div>
-            <h1>hello World</h1>
-        </div>
-    )
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
 }
+
+const Person = () => <h2>John Doe</h2>;
+
+const Message = () => {
+  return <p>This is my message.</p>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
