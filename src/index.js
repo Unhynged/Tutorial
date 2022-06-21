866b09b383afc3e3d971a6049d9334fdaf650ef4
Unhynged <unhynged@protonmail.com>
2022-06-21 18:16:15 +1000

    Simple List -> Proper List

--- src/index.js
+++ src/index.js
@@ -3,71 +3,47 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 // Setup Variables
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81F30JDZU9L._AC_UL604_SR604,400_.jpg",
-  title: "Why a Daughter Needs a Dad",
-  author: "Gregory E. Lang",
-};
+const books = [
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/81F30JDZU9L._AC_UL604_SR604,400_.jpg",
+    title: "Why a Daughter Needs a Dad",
+    author: "Gregory E. Lang",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/81a5KHEkwQL._AC_UL604_SR604,400_.jpg",
+    title: "Oh, the Places You'll Go!",
+    author: "Dr. Seuss",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/61sDK3mWV9L._AC_UL604_SR604,400_.jpg",
+    title: "I'd Like to Play Alone, Please: Essays",
+    author: "Tom Segura",
+  },
+];
 
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81a5KHEkwQL._AC_UL604_SR604,400_.jpg",
-  title: "Oh, the Places You'll Go!",
-  author: "Dr. Seuss",
-};
-
-const thirdBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/61sDK3mWV9L._AC_UL604_SR604,400_.jpg",
-  title: "I'd Like to Play Alone, Please: Essays",
-  author: "Tom Segura",
-};
-
+const names = ['john', 'peter', 'susan']
+const newNames = names.map((name) => {
+    return <h1>{name}</h1>
+});
+console.log(newNames);
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, sequi, libero nam error quis fuga inventore distinctio voluptatum esse sed veritatis adipisci aliquid ipsam totam, omnis perferendis earum illum placeat!
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-      <Book
-        img={thirdBook.img}
-        title={thirdBook.title}
-        author={thirdBook.author}
-      />
+        {names}
     </section>
   );
 }
 
 const Book = (props) => {
-    const {img, title, author, children} = props;
-    return (
-        <article className='book'>
-            <img className='img' src={img} alt='' />
-            <h1>{title}</h1>
-            <h4>{author}</h4>
-            {children}
-        </article>
-    );
+  const { img, title, author } = props;
+  return (
+    <article className="book">
+      <img className="img" src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
 };
-
-// const Book = ({img, title, author, children }) => {
-//     return (
-//         <article className='book'>
-//             <img className='img' src={img} alt='' />
-//             <h1>{title}</h1>
-//             <h4>{author}</h4>
-//             {children}
-//         </article>
-//     );
-// };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
