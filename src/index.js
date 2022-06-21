d47898eb31470b615e503d7565086a737455be8b
Unhynged <unhynged@protonmail.com>
2022-06-21 15:44:21 +1000

    Modified index.js, JSX Javascript -> Props

--- src/index.js
+++ src/index.js
@@ -8,41 +8,28 @@ function BookList() {
   return (
     <section className="booklist">
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
 
+const author = 'Gregory E. Lang'
 const Book = () => {
+  const title = "Why a Daughter Needs a Dad";
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/81F30JDZU9L._AC_UL604_SR604,400_.jpg"
+        alt=""
+        width="280px"
+        height="200px"
+      />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
+      {/* <p>This is how you comment out in JSX */}
+      <p>{6+6}</p>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/81F30JDZU9L._AC_UL604_SR604,400_.jpg"
-    alt=""
-    width="280px"
-    height="200px"
-  />
-);
-
-const Title = () => <h1>Why a Daughter Needs a Dad</h1>;
-
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    Gregory E. Lang
-  </h4>
-);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
