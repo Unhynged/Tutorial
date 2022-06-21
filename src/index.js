bef5ab90ebf95b7ff53e65da9afa85e1158fe5d5
Unhynged <unhynged@protonmail.com>
2022-06-21 15:34:17 +1000

    JSX CSS -> JSX Javascript

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // CSS Basics
-import './index.css';
+import "./index.css";
 
 function BookList() {
   return (
-    <section  className="booklist">
+    <section className="booklist">
       <Book />
       <Book />
       <Book />
@@ -19,7 +19,7 @@ function BookList() {
 
 const Book = () => {
   return (
-    <article className='book'>
+    <article className="book">
       <Image />
       <Title />
       <Author />
@@ -30,13 +30,19 @@ const Book = () => {
 const Image = () => (
   <img
     src="https://images-na.ssl-images-amazon.com/images/I/81F30JDZU9L._AC_UL604_SR604,400_.jpg"
-    alt="" width="280px" height="200px"
+    alt=""
+    width="280px"
+    height="200px"
   />
 );
 
 const Title = () => <h1>Why a Daughter Needs a Dad</h1>;
 
-const Author = () => <h4>Gregory E. Lang</h4>;
+const Author = () => (
+  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+    Gregory E. Lang
+  </h4>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
