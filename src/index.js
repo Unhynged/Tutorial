396c272ce9ecb5896d88854c3e966cb24092d38f
Unhynged <unhynged@protonmail.com>
2022-06-22 14:22:26 +1000

    Modified index.js, Event Basics -> Import and Export Statements

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import "./index.css";
 
 // Setup Variables
 const books = [
-  { 
+  {
     id: 1,
     img: "https://images-na.ssl-images-amazon.com/images/I/81F30JDZU9L._AC_UL604_SR604,400_.jpg",
     title: "Why a Daughter Needs a Dad",
     author: "Gregory E. Lang",
   },
-  { 
+  {
     id: 2,
     img: "https://images-na.ssl-images-amazon.com/images/I/81a5KHEkwQL._AC_UL604_SR604,400_.jpg",
     title: "Oh, the Places You'll Go!",
@@ -30,46 +30,67 @@ const books = [
   },
 ];
 
-// function BookList() {
-//   return (
-//     <section className="booklist">
-//       {books.map((book) => {
-//         return <Book key={book.id} book={book} />;
-//       })}
-//     </section>
-//   );
-// }
-
-// function BookList() {
-//     return (
-//         <section className='booklist'>
-//             {books.map((book, index) => {
-//                 return <Book key={index} book={book} />;
-//             })}
-//         </section>
-//     );
-// }
-
 function BookList() {
-    return (
-        <section className='booklist'>
-            {books.map((book, index) => {
-                return <Book key={book.id} {...book} ></Book>; {/* spread operator is "..." and then name of the operator. Now we're no longer passing in the book property as a object, we're passing it in as separate properties. That way don't need to use anymore the book property altogether. I would like to spread out all the properties here that are coming from the book on my prop. */}
-            })}
-        </section>
-    );
+  return (
+    <section className="booklist">
+      {books.map((book, index) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
+    </section>
+  );
 }
 
-// const Book = (props) => {
-//   const { img, title, author } = props;
+const Book = ({ img, title, author }) => {
+  // attribute, eventHandler
+  // The one thing that is different from the Javascript is that since we're setting this up in line, again, we need to make sure we're using the camelCase.
+  // onClick, onMouseOver
 
-const Book = ({ img, title, author}) => {
+  //   const clickHandler = () => {
+  //     alert("hello world");
+  //   };
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    alert("hello world");
+  };
+  const complexExample = () => {
+    console.log({ author });
+  };
+
+  //   return (
+  //     <article className="book">
+  //       <img className="img" src={img} alt="" />
+  //       <h1 onClick={() => console.log(title)}>{title}</h1>
+  //       <h4>{author}</h4>
+  //       <button type="button" onClick={clickHandler}>
+  //         reference example
+  //       </button>
+  //       <button type="button" onClick={complexExample}>
+  //         more complex example
+  //       </button>
+  //     </article>
+  //   );
+  // };
+  // Keep in mind here that both of them are doing the same thing right now. In one case we have the inline function that has the functionality. <h1 onClick={()=> console.log(title)}>{title}</h1> . In the second case, we have the reference. <button type="button" onClick={clickHandler}>reference example</button> .
+
+  // If you have the situation where you must pass the argument to into the function, you will have to first pass the argument, you'll have to setup a arrow function
 
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img className="img" src={img} alt="" />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        reference example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        more complex example
+      </button>
     </article>
   );
 };
